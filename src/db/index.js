0d2b880f5e22ef80e357d41d2788850671594a0f
tahfizmir <tahfeezmir27@gmail.com>
2024-12-20 12:50:25 +0530

    database connected

---
+++ src/db/index.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB = async () => {
+  try {
+    // mongoose.connect return an object.
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${DB_NAME}`
+    );
+    console.log(`\n MongoDB connected DB host: ${connectionInstance.connection.host}`);
+  } catch (error) {
+    console.log("MONGO DB connection error ", error);
+    process.exit(1);
+  }
+};
+export default connectDB;
