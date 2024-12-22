ac2322904a4103d3e641aaa610fbec78aaa8b571
tahfizmir <tahfeezmir27@gmail.com>
2024-12-22 19:50:52 +0530

    utilities coded

--- src/db/index.js
+++ src/db/index.js
@@ -14,3 +14,5 @@ const connectDB = async () => {
   }
 };
 export default connectDB;
+
+// async function returns promise
--- src/index.js
+++ src/index.js
@@ -2,11 +2,21 @@
 import dotenv from 'dotenv'
 
 import connectDB from './db/index.js';
-
+import { app } from './app.js';
 dotenv.config({
     path:'./env'
+});
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log('app listening on : ',process.env.PORT);
+        
+    })
+
+}).catch((error)=>{
+    console.log('src/index.js db connection failed with error :',error);
+    
 })
-connectDB();
 
 
 
