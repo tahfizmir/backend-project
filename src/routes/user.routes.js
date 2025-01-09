ceb105a5e86a9c531e2a0794c8003e319926e4ae
tahfizmir <tahfeezmir27@gmail.com>
2025-01-09 13:48:45 +0530

    routes defined for some of the user methods

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,28 +1,52 @@
 import { Router } from "express";
-import { loginUser, registerUser , logoutUser, refreshAccessToken} from "../controllers/user.controller.js";
-import { upload } from "../middleware/multer.middleware.js"
+import {
+  loginUser,
+  registerUser,
+  logoutUser,
+  refreshAccessToken,
+  changeCurrentPassword,
+  getCurrentUser,
+  updateAccountDetails,
+  updateUserAvatar,
+  updateUserCoverImage,
+  getUserChannelProfile,
+} from "../controllers/user.controller.js";
+import { upload } from "../middleware/multer.middleware.js";
 import { verifyJWT } from "../middleware/auth.middleware.js";
 
-const router = Router()
+const router = Router();
 
-router.route("/register").post(upload.fields([
+router.route("/register").post(
+  upload.fields([
     {
-        name:"avatar",
-        maxCount:1
+      name: "avatar",
+      maxCount: 1,
     },
     {
-        name:"coverImage",
-        maxCount:1
+      name: "coverImage",
+      maxCount: 1,
+    },
+  ]),
+  registerUser
+); // this register is suffix of the url from the middleware it is accessed from  eg users/register
 
-    }
-]),registerUser)  // this register is suffix of the url from the middleware it is accessed from  eg users/register
-
-router.route("/login").post(loginUser)
-
+router.route("/login").post(loginUser);
 
 // secured routes , user logged in hona chahiye
 
-router.route("/logout").post(verifyJWT, logoutUser) // do method like hai isiliye next likhte hai warna router confuse hota hai pehle konsa run karna hai 
-router.route("/refresh-route").post(refreshAccessToken) // verify wagiara sab method ke ander hi kiya hai 
+router.route("/logout").post(verifyJWT, logoutUser); // do method like hai isiliye next likhte hai warna router confuse hota hai pehle konsa run karna hai
+router.route("/refresh-route").post(refreshAccessToken); // verify wagiara sab method ke ander hi kiya hai
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+router.route("update-account").patch(verifyJWT, updateAccountDetails);
+router
+  .route("/avatar")
+  .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+router
+  .route("/cover-image")
+  .patch(verifyJWT, upload.single("/coverImage"), updateUserCoverImage);
+// since we are using params in getChannelProfile we need to write username in url
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
+router.route("/history").get(verifyJWT, getWatchHistory);
 
-export default router
+export default router;
